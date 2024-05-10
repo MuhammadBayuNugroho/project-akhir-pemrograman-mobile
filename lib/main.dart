75ea0947d21123dbefb3d631224559c67c508811
MuhammadBayuNugroho <bayu27.ngrh@gmail.com>
2024-05-10 21:37:03 +0700

    UTS-Page2-Product

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_app/pages/page1.dart';
 import 'package:flutter_app/pages/page2.dart';
 
-
 void main() {
   runApp(const MyApp());
 }
@@ -19,7 +18,6 @@ class MyApp extends StatelessWidget {
       routes: {
         '/page1': (context) => const Page1(),
         '/page2': (context) => const Page2(),
-       
       },
     );
   }
